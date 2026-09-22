6cb9cd9a35351297811812a744897496591f7e11
reghuagrasala <87054723+reghuagrasala@users.noreply.github.com>
2026-09-22 13:44:31 +0530

    Refresh app after address weather and globe fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="my-location-info-v18";
+const CACHE="my-location-info-v19";
 const LOCAL_ASSETS=["./","./index.html","./styles.css","./manifest.json","./js/app.js","./js/globe.js","./js/gps.js","./js/coordinates.js","./js/astronomy.js","./js/digipin.js","./js/storage.js","./js/compass.js","./js/units.js","./js/services/weather.js","./js/services/location.js","./icons/icon.svg"];
 const EXTERNAL_ASSETS=["https://cdn.jsdelivr.net/npm/three@0.179.1/build/three.min.js","https://cdn.jsdelivr.net/npm/globe.gl@2.46.2/dist/globe.gl.min.js","https://unpkg.com/three-globe/example/img/earth-blue-marble.jpg","https://unpkg.com/three-globe/example/img/earth-topology.png","https://unpkg.com/three-globe/example/img/night-sky.png","https://api.open-meteo.com/v1/forecast"];
 self.addEventListener("install",event=>{event.waitUntil((async()=>{const cache=await caches.open(CACHE);await cache.addAll(LOCAL_ASSETS);await Promise.all(EXTERNAL_ASSETS.map(async url=>{try{const r=await fetch(new Request(url,{mode:"cors",cache:"reload"}));if(r.ok)await cache.put(url,r)}catch{}}));await self.skipWaiting()})())});
